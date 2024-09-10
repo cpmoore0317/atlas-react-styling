8a15217cb96268a5bf7678b4b51e2148af22683f
cpmoore0317 <cpmoore317@gmail.com>
2024-09-10 15:26:26 -0500

    Adjusting dimensions for App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import MusicPlayer from "./MusicPlayer";
 
 function App() {
   return (
-    <div className="h-full flex flex-col justify-between p-8 min-h-screen">
-      <MusicPlayer />
+    <div className="min-h-screen flex flex-col items-center p-8">
+      <div className="w-full max-w-6xl">
+        <MusicPlayer />
+      </div>
       <Footer />
     </div>
   );
