69641f46875d2fcc3a6802ca46c4be4aff616ad0
cpmoore0317 <cpmoore317@gmail.com>
2024-09-10 15:18:41 -0500

    Switched to a light theme

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -4,7 +4,7 @@ import Playlist from "./components/Playlist";
 
 export default function MusicPlayer() {
   return (
-    <div className="max-w-4xl mx-auto p-4 bg-dark-gray text-light-gray rounded-lg shadow-lg">
+    <div className="max-w-4xl mx-auto p-4 bg-white text-black rounded-lg shadow-thin">
       <div className="flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8">
         <div className="flex-1">
           <CurrentlyPlaying />
