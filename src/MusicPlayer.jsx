319d6167c62457cdf11f84b1b52a71c3f7afdd51
cpmoore0317 <cpmoore317@gmail.com>
2024-09-10 14:01:17 -0500

    Layout adjustment for screen width change

--- src/MusicPlayer.jsx
+++ src/MusicPlayer.jsx
@@ -1,41 +1,22 @@
 import React from "react";
-import CoverArt from "./components/CoverArt";
-import SongTitle from "./components/SongTitle";
-import PlayControls from "./components/PlayControls";
-import VolumeControl from "./components/VolumeControl";
-import PlayListItem from "./components/PlayListItem";
-
-// Example song data for demonstration
-const currentSong = {
-  imageUrl: "./assets/placeholder.svg",
-  altText: "Cover Art",
-  title: "Song Title",
-  author: "Artist Name",
-  genre: "Pop",
-  length: "3:45"
-};
+import CurrentlyPlaying from "./components/CurrentlyPlaying";
+import Playlist from "./components/Playlist";
 
 export default function MusicPlayer() {
   return (
-    <div className="max-w-2xl mx-auto p-4 bg-gray-800 text-white rounded-lg shadow-lg space-y-8">
-      {/* Cover Art and Song Title */}
-      <div className="flex flex-col items-center space-x-4">
-        <CoverArt imageUrl={currentSong.imageUrl} altText={currentSong.altText} />
-        <SongTitle title={currentSong.title} author={currentSong.author} />
+    <div className="max-w-4xl mx-auto p-4 bg-gray-900 text-white rounded-lg shadow-lg">
+      {/* Flex container that changes layout based on screen size */}
+      <div className="flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8">
+        {/* Currently Playing section */}
+        <div className="flex-1">
+          <CurrentlyPlaying />
+        </div>
+
+        {/* Playlist section */}
+        <div className="flex-1">
+          <Playlist />
+        </div>
       </div>
-
-      {/* Play Controls */}
-      <PlayControls />
-
-      {/* Volume Control */}
-      <VolumeControl />
-
-      {/* Playlist */}
-      <div className="flex flex-col items-center space-y-4"> 
-        {/* Vertical spacing instead of horizontal */}
-        <SongTitle title={currentSong.title} author={currentSong.author} />
-      </div>
-
     </div>
   );
 }
